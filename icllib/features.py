61a6e97865db718afdedb2b9a7aafa11c5d1bcfa
Mika Tammi <mikatammi@gmail.com>
2016-02-12 15:24:46 +0200

    Implemented Feature: Distance between vector and points
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- icllib/features.py
+++ icllib/features.py
@@ -1,12 +1,23 @@
+"""Collection of functions related to feature extraction from gazedata."""
 import numpy as np
 
 
 def distance_between_points(points):
-    """Calculate D-dimensional euclidean distance between vertices
+    """Calculate D-dimensional euclidean distance between vertices.
 
-       Input NxD NumPy array, where D is dimensions.
+    Input NxD NumPy array, where D is dimensions.
     """
-
     diff = points[1:] - points[:-1]
 
     return np.sqrt(np.power(diff, 2).sum(axis=1)).sum()
+
+
+def distance_between_vector_and_point(vec, point):
+    """Calculate distance for each element in vector vec from point.
+
+    Input vec NxD Numpy array of vectors,
+          points D Numpy vector,
+    """
+    diff = vec - point
+
+    return np.sqrt(np.power(diff, 2).sum(axis=1))
--- tests/test_features.py
+++ tests/test_features.py
@@ -1,9 +1,13 @@
+"""Tests for functions in features module."""
 import unittest2 as unittest
 import numpy as np
 
 
 class TestFeatures(unittest.TestCase):
+    """Test case for functions in features module."""
+
     def test_distance_between_points(self):
+        """Test disstance between points with few simple test vectors."""
         from icllib.features import distance_between_points
 
         arr = np.array([[0.0, 0.0], [1.0, 0.0], [0.0, 0.0]])
@@ -13,3 +17,17 @@ class TestFeatures(unittest.TestCase):
                          [0.0, 1.0, 0.0],
                          [0.0, 0.0, 0.0]])
         self.assertEqual(distance_between_points(arr2), 2.0)
+
+    def test_distance_between_vector_and_point(self):
+        """Test distance between vector and points with simple test vector."""
+        from icllib.features import distance_between_vector_and_point
+
+        arr2 = np.array([[0.0, 0.0, 0.0],
+                         [0.0, 1.0, 0.0],
+                         [0.0, 0.0, 0.0]])
+        p = np.array([0.0, 0.0, 0.0])
+        dists = np.array([0.0, 1.0, 0.0])
+
+        self.assertEqual(
+            np.array_equal(distance_between_vector_and_point(arr2, p), dists),
+            True)
