c758288aa43eba30e8691e393d4f44751b47f867
Mika Tammi <mikatammi@gmail.com>
2016-03-03 20:19:44 +0200

    Moved dataset module into its own subdirectory
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

---
+++ icllib/dataset/__init__.py
@@ -0,0 +1,12 @@
+"""Dataset module."""
+
+__all__ = (
+    'DatasetError',
+    'Dataset',
+    'CSVDataset',
+    'TrialIterator',
+    'TBTFile',
+    'GazedataFile'
+)
+
+from .dataset import *  # noqa
