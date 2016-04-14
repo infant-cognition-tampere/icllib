1b16e5e258ff74db7c506fa0e9a582889c29d294
Mika Tammi <mikatammi@gmail.com>
2016-04-14 17:40:23 +0300

    Added script which replicates Python 2.7 bug when reading TREC2 dataset
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

---
+++ bin/trec2_bug.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""Converts TREC2 dataset to HDF5 format."""
+
+from icllib.dataset import CSVProtocolDataset
+from icllib.dataset.protocol import TREC2
+from os.path import join
+
+
+if __name__ == "__main__":
+
+    # Load dataset into memory
+    basedir = '/Users/mika/ICL/FPA data'
+    ds = CSVProtocolDataset(join(basedir, 'TREC2'), TREC2())
+
+    def read_gazedata(gzdname):
+        print('==== Reading %s' % gzdname)
+        return ds.get_gazedata(gzdname)
+
+    dslist = ['Disengagement201-272-221-2.gazedata']
+    gazedatas = map(read_gazedata, dslist)
