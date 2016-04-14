89b70a02fd5541c1cc00220e1cc48fbfcba9f4d8
Mika Tammi <mikatammi@gmail.com>
2016-04-14 17:45:36 +0300

    Fixed docstring convention error in trec2_bug.py script
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- bin/trec2_bug.py
+++ bin/trec2_bug.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     basedir = '/Users/mika/ICL/FPA data'
     ds = CSVProtocolDataset(join(basedir, 'TREC2'), TREC2())
 
-    def read_gazedata(gzdname):
+    def _read_gazedata(gzdname):
         print('==== Reading %s' % gzdname)
         return ds.get_gazedata(gzdname)
 
     dslist = ['Disengagement201-272-221-2.gazedata']
-    gazedatas = map(read_gazedata, dslist)
+    gazedatas = map(_read_gazedata, dslist)
